7eae159b902aad819c081379a3d63aa7a4140501
heetaeda <eogh3179@gmail.com>
2025-04-06 18:52:13 +0900

    comma added

--- code/calculator_final.py
+++ code/calculator_final.py
@@ -8,7 +8,7 @@ from PyQt5 import uic
 # git test code 
 
 # from_class = uic.loadUiType("/home/yoon/ws/PyQt/src/calculator_copy.ui")[0]
-from_class = uic.loadUiType("../ui/calculator_copy.ui")[0]
+from_class = uic.loadUiType("./ui/calculator_copy.ui")[0]
 
 # 화면 클래스 구상
 class WindowClass(QMainWindow, from_class):
@@ -60,6 +60,8 @@ class WindowClass(QMainWindow, from_class):
         self.result_screen.setText(str(self.digit_result))
         self.result_screen.setStyleSheet("QLineEdit { font-size: 24px; }")
 
+        self.result_screen.textChanged.connect(self.display_digit)
+
         # 덧셈 버튼 클릭 시
         self.plus_color_changed = False  # 버튼 색상 플래그
         self.plus_btn.clicked.connect(lambda: self.operator_btn_Clicked('+'))
@@ -100,6 +102,34 @@ class WindowClass(QMainWindow, from_class):
         self.digit_8.clicked.connect(lambda: self.digit_Clicked('8'))
         self.digit_9.clicked.connect(lambda: self.digit_Clicked('9'))
 
+    # self.result_screen 값이 변할 때마다 해당 함수 실행
+    def display_digit(self):
+        if self.digit_result == "-0":
+            str_format = self.digit_result
+        else:
+            try:
+                str_format = str(format(int(self.digit_result), ','))
+            except:
+                str_format = str(format(float(self.digit_result), ','))
+
+            if str_format[-2:] == ".0":
+                # str_format = str(formatted)
+                str_format = str_format[:-1]
+
+        self.result_screen.setText(str(str_format))
+
+        # str_digit = str(digit_result)
+        # if '.' in str_digit:
+        #     idx = str_digit.index('.')
+        #     str_int = str_digit[:idx]
+        #     str_demical = str_digit[idx:]
+        # else:
+        #     str_int = str_digit
+
+        # len_digit = len(str_digit) - 1
+        # if len_digit // 3 > 0:
+
+            
 
     def digit_zero_Clicked(self, digit):
         self.btn_color_Reset()
@@ -124,6 +154,7 @@ class WindowClass(QMainWindow, from_class):
             self.digit_result += digit
     
         self.result_screen.setText(str(self.digit_result))
+        
         self.is_last_input_operator = False  # 마지막 입력이 연산자가 아님
 
         # 각 리셋 변수 초기화
@@ -179,6 +210,8 @@ class WindowClass(QMainWindow, from_class):
             interim_list = self.calc_list[last_digit_idx:self.operator_cnt]
             interim_str = ''.join(interim_list)
 
+            interim_str = interim_str.replace(',', '')
+
             self.digit_result_num = round(eval(interim_str), 8)
 
             if int(self.digit_result_num) == self.digit_result_num:  # 정수형으로 나타낼 수 있는데 실수형으로 나타내진 경우
@@ -198,6 +231,9 @@ class WindowClass(QMainWindow, from_class):
             last_digit_idx = self.operator_cnt - 3  # 이전 숫자부터 선택 
             interim_list = self.calc_list[last_digit_idx:self.operator_cnt]
             interim_str = ''.join(interim_list)
+
+            interim_str = interim_str.replace(',', '')
+
             if not self.calc_list[self.operator_cnt - 2] in ['*', '/']:  # 이전 연산자가 곱셈이나 나눗셈이 아닐 경우 cnt초기화 안되고 cnt = 5 가 될 것이다.
                 self.digit_result = self.calc_list[self.operator_cnt - 1]
                 self.result_screen.setText((self.digit_result))  # 이전 클릭된 숫자 보여줌
@@ -328,6 +364,8 @@ class WindowClass(QMainWindow, from_class):
         if self.is_last_input_operator == False:  # 마지막 입력이 연산자가 아닌 경우 즉, 숫자로 끝난 경우
             self.calc_list.append(self.digit_result)  # 현재 숫자를 계산식에 추가
             self.calc_str = ''.join(self.calc_list)  # 문자열로 변환
+
+            self.calc_str = self.calc_str.replace(',', '')
     
             self.digit_result_num = (round(eval(self.calc_str), 8))
 
@@ -363,7 +401,7 @@ class WindowClass(QMainWindow, from_class):
         self.calc_list = []  # 계산 리스트 초기화
         self.is_init = True
         self.is_last_input_operator = False
-        self.digit_clear_flag = False
+        # self.digit_clear_flag = False
         self.operator_clear_flag = False 
 
 
@@ -375,15 +413,18 @@ class WindowClass(QMainWindow, from_class):
 
         else:  # self.clear_btn.text() != 'AC' 즉, 'C' 일 때
             self.digit_result = '0'
-            #self.result_screen.setText(str(self.digit_result))  # 우선 화면에 0 을 띄워주고
-            self.clear_btn.setText('AC')  # 'AC' 로 글자 초기화
-            btn_status_dict = {'+' : 'self.plus_color_changed', '-' : 'self.minus_color_changed',
-                                '*' : 'self.multiply_color_changed', '/' : 'self.divide_color_changed'}
-            if eval(btn_status_dict[self.calc_list[-1]]) != True:  # 숫자 누른 상태에서 클리어 누르면
-                self.is_new_input = True  # 새롭게 문자열 입력
-                # 마지막에 눌렀던 연산자 버튼 켜주고
-                key_value = self.calc_list[-1]  # 예를 들어 '+' 라면 + 버튼을 다시 켜야 함.
-                self.btn_color_Changed(key_value)  # 생성된 딕셔너리에서 키값을 이용해 값 가져옴.
+            try:
+                #self.result_screen.setText(str(self.digit_result))  # 우선 화면에 0 을 띄워주고
+                self.clear_btn.setText('AC')  # 'AC' 로 글자 초기화
+                btn_status_dict = {'+' : 'self.plus_color_changed', '-' : 'self.minus_color_changed',
+                                    '*' : 'self.multiply_color_changed', '/' : 'self.divide_color_changed'}
+                if eval(btn_status_dict[self.calc_list[-1]]) != True:  # 숫자 누른 상태에서 클리어 누르면
+                    self.is_new_input = True  # 새롭게 문자열 입력
+                    # 마지막에 눌렀던 연산자 버튼 켜주고
+                    key_value = self.calc_list[-1]  # 예를 들어 '+' 라면 + 버튼을 다시 켜야 함.
+                    self.btn_color_Changed(key_value)  # 생성된 딕셔너리에서 키값을 이용해 값 가져옴.
+            except:
+                self.reset_calculator()
         self.result_screen.setText(str(self.digit_result))
 
 
